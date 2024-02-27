fc29d54331b3da19edae116acaa538a92eee2060
Scathies <Scathiesmail@gmail.com>
2024-02-27 15:49:58 +0300

    add postman test

--- src/main/java/scathies/tennis/controller/ExceptionHandlerController.java
+++ src/main/java/scathies/tennis/controller/ExceptionHandlerController.java
@@ -1,5 +1,7 @@
 package scathies.tennis.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,11 +11,13 @@ import org.springframework.web.method.annotation.HandlerMethodValidationExceptio
 public class ExceptionHandlerController {
 
     @ExceptionHandler(Exception.class)
-    public String handleException(Exception exception, Model model) {
+    public String handleException(Exception exception, Model model, HttpServletResponse resp) {
         if (exception instanceof HandlerMethodValidationException e) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             model.addAttribute("reason", "имя игрока/ов не должно быть пустым");
             return "new-match-form";
         } else if (exception instanceof IllegalArgumentException) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             model.addAttribute("reason", exception.getMessage());
             return "new-match-form";
         }
